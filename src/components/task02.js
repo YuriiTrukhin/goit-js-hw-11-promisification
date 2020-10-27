f7a99331ccd96f1f22a7ad9d849cdd344efa1b72
YuriiTrukhin <68285301+YuriiTrukhin@users.noreply.github.com>
2020-10-27 21:15:57 +0200

    home work 11

---
+++ src/components/task02.js
@@ -0,0 +1,37 @@
+export default function () {
+    const fn = function(){
+    const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+    const toggleUserState = (allUsers, userName) => {        
+      const  updatedUsers = allUsers.map(user =>
+    user.name === userName ? { ...user, active: !user.active } : user,
+  );
+return new Promise((res, rej) => {
+        setTimeout(() => {
+                  res(updatedUsers) 
+      },1000)
+  })
+  
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+/*
+ * Сейчас работает так
+ */
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
+}    
+return {fn}
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import css from "./css/style.css";
+import taskOne from "./components/task01.js";
+import taskTwo from "./components/task02.js";
+import taskThree from "./components/task03.js";
+
+taskOne().delay()
+taskTwo().fn()
+taskThree().fn()
